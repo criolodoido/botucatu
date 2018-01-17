90b817f43804a7fe9b3411a1f4cb3fb37c666812
criolodoido <victor.santos.cd@gmail.com>
2018-01-17 11:46:23 -0200

    post

--- pet/urls.py
+++ pet/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
 	url(r'^$', views.petmania, name='petmania'),
-	url(r'^petmania/(?P<pk>[0-9]+)/$', views.post_detail, name='detalhe'),
+	url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='detalhe'),
 	url(r'^comida$', views.comida, name='comida'),
 	url(r'^educacao$', views.educacao, name='educacao'),
 	url(r'^acessorios$', views.acessorios, name='acessorios'),
